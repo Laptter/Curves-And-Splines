cff0f1e28075c4b36bca761900a980a6580fa5f6
Laptter <jxl1553519884@gmail.com>
2020-09-11 17:10:54 +0800

    Project init

---
+++ Assets/Scripts/BezierSpline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierSpline : MonoBehaviour
+{
+    public Vector3[] points;
+
+    public Vector3 GetPoint(float t)
+    {
+        return Bezier.GetPoint(points[0], points[1], points[2], points[3], t);
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        return Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t);
+    }
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
+
+    public void AddCurve()
+    {
+        Vector3 point = points[points.Length - 1];
+        Array.Resize(ref points, points.Length + 3);
+        point.x += 1f;
+        points[points.Length - 3] = point;
+        point.x += 1f;
+        points[points.Length - 2] = point;
+        point.x += 1f;
+        points[points.Length - 1] = point;
+    }
+}
